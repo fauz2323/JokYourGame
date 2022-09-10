7817e0f31662ae921095761e0c5f2b17f7ac254f
Zen <59063107+fauz2323@users.noreply.github.com>
2022-09-10 23:23:22 +0700

    add

--- lib/app/modules/home/view/screen/about_us.dart
+++ lib/app/modules/home/view/screen/about_us.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:joki_apps/app/modules/home/controller/home_controller.dart';
 
 class AboutUs extends StatelessWidget {
@@ -34,8 +35,8 @@ class AboutUs extends StatelessWidget {
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                    child: Obx(
+                      () => Text(homeCOntroller.about.value),
                     ),
                   ),
                 ],
